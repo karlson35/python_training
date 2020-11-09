50811fd40a82021d66f34a0dfe1ac2f30212ad20
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2020-11-10 00:12:36 +0300

    Валидация фикстуры перед каждым тестом

--- fixture/application.py
+++ fixture/application.py
@@ -21,3 +21,9 @@ class Application:
     def destroy(self):
         self.wd.quit()
 
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
