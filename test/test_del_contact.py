47ca2f52344b2c1f0ab2fa890bd03c274f9f8e4a
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2020-11-14 20:50:06 +0300

    Фикс удаления первого контакта

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,5 +1,5 @@
 __author__ = 'Igor Nikolaev'
-from model.contact import Contact
+import time
 
 
 def test_delete_first_contact(app):
@@ -7,6 +7,7 @@ def test_delete_first_contact(app):
         app.contact.create_empty()
     old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    time.sleep(1)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts[0:1] = []
