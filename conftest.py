d2fe0e310cb6fb27a43f447e79c6bbd17c1cd6df
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2020-11-26 18:13:01 +0300

    Реализованы опции командной строки, позволяющие указывать тип браузера, адрес тестируемого приложения и данные для входа в приложение

--- conftest.py
+++ conftest.py
@@ -9,12 +9,18 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+    username = request.config.getoption("--username")
+    password = request.config.getoption("--password")
+    if username is None or password is None:
+        raise ValueError("Please, enter valid credentials\nUsage: --username=""username"" --password=""password""")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
+    fixture.session.ensure_login(username=username, password=password)
     return fixture
 
 
@@ -25,3 +31,10 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--username", action="store")
+    parser.addoption("--password", action="store")
