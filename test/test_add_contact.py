8f0be0dd6e1aa169d316b6e8ca2b9a4a45bd2447
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2020-11-30 15:45:41 +0300

    Отделение тестовых данных от сценариев

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,38 +2,7 @@
 from model.contact import Contact
 import pytest
 import random
-import string
-import calendar
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_tel(maxlen):
-    symbols = string.digits + "()- "
-    return "".join([random.choice(symbols) for i in range(maxlen)])
-
-
-testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="",
-                    tel_home="", tel_mobile="", tel_work="", tel_fax="", email1="", email2="", email3="", homepage="",
-                    bday="", bmonth="-", byear="", aday="", amonth="-", ayear="", group="[none]", address2="",
-                    phone2="", notes="")] + [
-               Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
-                       lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
-                       title=random_string("title", 10), company=random_string("company", 10),
-                       address=random_string("address", 10), tel_home=random_tel(10),
-                       tel_mobile=random_tel(10), tel_work=random_tel(10), tel_fax=random_tel(10),
-                       email1=random_string("email1", 10), email2=random_string("email2", 10),
-                       email3=random_string("email3", 10), homepage=random_string("homepage", 10),
-                       bday=str(random.randrange(31)), bmonth=random.choice(calendar.month_name[1:13]),
-                       byear=random.randrange(1900, 2020), aday=str(random.randrange(31)),
-                       amonth=random.choice(calendar.month_name[1:13]), ayear=random.randrange(1900, 2050),
-                       group="random", address2=random_string("address2", 10), phone2=random_tel(10),
-                       notes=random_string("notes", 10))
-               for i in range(5)
-           ]
+from data.add_contact import testdata
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
