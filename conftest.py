e7f71dec516040969ca427ed4e4967a5859d3fce
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2020-11-30 15:32:39 +0300

    Реализована загрузка информации о тестируемом приложении из конфигурационного файла, и опция командной строки, позволяющая передавать путь к этому конфигурационному файлу.

--- conftest.py
+++ conftest.py
@@ -1,26 +1,26 @@
 __author__ = 'Igor Nikolaev'
 
 import pytest
+import json
+import os.path
 from fixture.application import Application
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    username = request.config.getoption("--username")
-    password = request.config.getoption("--password")
-    if username is None or password is None:
-        raise ValueError("Please, enter valid credentials\nUsage: --username=""username"" --password=""password""")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username=username, password=password)
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -35,6 +35,7 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--username", action="store")
-    parser.addoption("--password", action="store")
+    parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--username", action="store", default="target.json")
+    parser.addoption("--password", action="store", default="target.json")
+
