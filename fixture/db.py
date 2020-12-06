c1dd418eafc300aac5d527ec69803c655a952137
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2020-12-06 15:00:19 +0300

    Загрузка информации из базы данных в виде объектов

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,9 @@
 __author__ = 'Igor Nikolaev'
+
 import pymysql
+
+from model.contact import Contact
+from model.group import Group
 
 
 class DbFixture:
@@ -9,6 +13,39 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        group_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return group_list
+
+    def get_contact_list(self):
+        contact_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, middlename, nickname, company, title, address, home, "
+                           "mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, "
+                           "ayear, address2, phone2, notes from addressbook")
+            for row in cursor:
+                (id, firstname, lastname, middlename, nickname, company, title, address, home,
+                 mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth,
+                 ayear, address2, phone2, notes) = row
+                contact_list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, middlename=middlename,
+                                            nickname=nickname, company=company, title=title, address=address,
+                                            tel_home=home, tel_mobile=mobile, tel_work=work, tel_fax=fax, email1=email,
+                                            email2=email2, email3=email3, homepage=homepage, bday=bday, bmonth=bmonth,
+                                            byear=byear, aday=aday, amonth=amonth, ayear=ayear, address2=address2,
+                                            phone2=phone2, notes=notes))
+        finally:
+            cursor.close()
+        return contact_list
 
     def destroy(self):
         self.connection.close()
---
+++ test/test_db_matches_ui.py
@@ -0,0 +1,23 @@
+__author__ = 'Igor Nikolaev'
+
+from model.contact import Contact
+from model.group import Group
+
+
+def test_group_list(app, db):
+    ui_list = app.group.get_group_list()
+
+    def clean(group):
+        return Group(id=group.id, name=group.name.strip())
+    db_list = list(map(clean, db.get_group_list()))
+    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+
+    def clean(contact):
+        return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip(),
+                       middlename=contact.middlename.strip(), nickname=contact.nickname.strip())
+    db_list = list(map(clean, db.get_contact_list()))
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
